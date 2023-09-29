5196fcb34c32d50fe6d6ae822073e203b42057a1
ChrisDTon <chranieon@gmail.com>
2023-09-29 13:02:38 +0200

    onclick events and code readablitiy

--- herdmentality/src/app/components/ModifierButtons.tsx
+++ herdmentality/src/app/components/ModifierButtons.tsx
@@ -4,8 +4,14 @@ import React from 'react'
 const ModifierButtons = () => {
   return (
     <div className='grid p-2 grid-cols-2'>
-        <button className='btn-success rounded-l-xl text-2xl font-medium p-2 hover:bg-[#5ab9a1]'>Point</button>
-        <button className='btn-error rounded-r-xl text-2xl font-medium p-2 hover:bg-[#ad1423]'>Black Sheep</button>
+        <button
+          className='btn-success rounded-l-xl text-2xl font-medium p-2 hover:bg-[#5ab9a1]'
+          onClick={() => console.log('point')}
+        >Point</button>
+        <button
+          className='btn-error rounded-r-xl text-2xl font-medium p-2 hover:bg-[#ad1423]'
+          onClick={() => console.log('black sheep')}
+        >Black Sheep</button>
     </div>
   )
 }
