39469d69d435e323a0d768f827ec9e4d7579ed83
ChrisDTon <chranieon@gmail.com>
2023-10-02 11:38:06 +0200

    Functioning database

---
+++ prisma/saveUserScript.ts
@@ -0,0 +1,28 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+async function main() {
+  const user = await prisma.user.create({
+    data: {
+      name: 'Alice',
+      score: 0,
+      Round: {
+        create: {
+          point: false,
+        },
+      },
+    },
+  })
+  console.log(user)
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
